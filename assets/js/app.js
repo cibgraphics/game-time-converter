8ac7ee8bdba4dc41d86162ce1b1730b544671e70
Christopher Bishop <cbishop@solutionstream.com>
2017-11-20 15:22:01 -0700

    no less, better design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,44 @@
 $(function() {
+
+  // Properly Style select inputs
+  $("select").uniform();
     
-  $(function(){
-    
-    // Settings and Variables
-    var $userbase = parseInt($('.base').val()),
-        $userminute = parseInt($('.minutes').val()),
-        $userseconds = parseInt($('.seconds').val());
-    
-    $('.base, .minutes, .seconds').on('change', function(){
-      $userbase = parseInt($('.base').val());
+  // Settings and Variables
+  var $userbase = parseInt($('.base').val()),
       $userminute = parseInt($('.minutes').val()),
       $userseconds = parseInt($('.seconds').val());
-      calculateTime($userbase, $userminute, $userseconds);
-    });
-    
+  
+  $('.base, .minutes, .seconds').on('change', function(){
+    var $userbase = parseInt($('.base').val()),
+    $userminute = parseInt($('.minutes').val()),
+    $userseconds = parseInt($('.seconds').val());
     calculateTime($userbase, $userminute, $userseconds);
-    
-    // Convert Minutes to Milliseconds
-    function minuteToMS(minute) {
-      return Math.floor(minute*1000*60);
-    }
-    
-    // Convert Seconds to Milliseconds  
-    function secondsToMS(seconds) {
-      return Math.floor(seconds*1000);
-    }
-    
-    // Do some fancy Math to turn Milliseconds into redable human format.
-    function calculateTime(base, minutes, seconds) {
-      var $time = $('time'),
-          msUserBase = minuteToMS($userbase),
-          msUserMinute = minuteToMS($userminute),
-          msUserSeconds = secondsToMS($userseconds),
-          totalMS = msUserBase - (msUserMinute + msUserSeconds),
-          minutes = Math.floor(totalMS / 60000),
-          seconds = ((totalMS % 60000) / 1000).toFixed(0);
-      
-      // Export out Result
-      $time.text(seconds == 60 ? (minutes+1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
-    }; 
   });
+  
+  calculateTime($userbase, $userminute, $userseconds);
+  
+  // Convert Minutes to Milliseconds
+  function minuteToMS(minute) {
+    return Math.floor(minute*1000*60);
+  }
+  
+  // Convert Seconds to Milliseconds  
+  function secondsToMS(seconds) {
+    return Math.floor(seconds*1000);
+  }
+  
+  // Do some fancy Math to turn Milliseconds into redable human format.
+  function calculateTime(base, minutes, seconds) {
+    var $time = $('time'),
+        msUserBase = minuteToMS(base),
+        msUserMinute = minuteToMS(minutes),
+        msUserSeconds = secondsToMS(seconds),
+        totalMS = msUserBase - (msUserMinute + msUserSeconds),
+        calminutes = Math.floor(totalMS / 60000),
+        calseconds = ((totalMS % 60000) / 1000).toFixed(0);
+    
+    // Export out Result
+    $time.text(calseconds == 60 ? (calminutes+1) + ":00" : calminutes + ":" + (calseconds < 10 ? "0" : "") + calseconds);
+  }
   
 });
